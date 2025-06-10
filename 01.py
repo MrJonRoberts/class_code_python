2b9e04aa89f3c47c06b36c5d88b9e99db11d82d9
jroberts <jroberts@tas.qld.edu.au>
2025-06-10 12:44:53 +1000

    01

---
+++ 01.py
@@ -0,0 +1,54 @@
+# Task 1: Fix These Statements
+print(Hello World)
+
+Print("Python")
+
+print "Coding"
+
+# Task 2: Write a 2 line joke with a blank line in between
+
+
+# Task 3
+# ask the user what country they live in.
+# print out a response using their input
+# eg: user inputs 'Australia'
+# output: Wow, I live in Australia too!  (using the variable)
+
+# Task 4
+# ask the user for two numbers (use 2 input statements)
+# output the sum of the two numbers using the variables
+
+
+# Mini Chat-Bot Project
+# Goal: Create interactive program
+
+#Steps:
+#1. Ask user's name
+#2. Greet them
+#3. Ask questions (at least 2)
+#4. Respond using their answers
+
+
+###### Extension Activities
+# Add more questions
+# Include blank lines
+# Convert numeric inputs
+# Add basic calculations
+
+
+############################## Exit Ticket Questions:
+
+#1. What is an algorithm?
+#2. What does print() do?
+#3. How do we get user input?
+#4. What is a variable?
+
+##########################################################
+# Complete Chat-Bot if needed
+# Complete Exit Ticket Questions.
+
+# Add these features to the chatbot.
+# At least 3 questions
+#One numeric input
+# One calculation
+# Summary of all answers
---
+++ Notes/01_notes.py
@@ -0,0 +1,94 @@
+# print
+
+print("Hello World!")
+
+print("I can put anything between the quotation marks")
+
+print("Print is not the same as print")
+
+print('you can use double quotes or single quotes.')
+
+
+# print examples
+print("Hello, World!")
+
+print("I love coding!")
+
+print()  # Creates blank lineprint("Python is fun!")
+
+
+# print mistakes
+
+Print("Hello") # ❌ upper case P
+print "hello"  # ❌ missing parentheses -- brackets ()
+print(hello)   # ❌ missing quotes " "
+print("Hello ✅") # correct
+
+
+# Task 1: Fix These Statements
+print(Hello World)
+
+Print("Python")
+
+print "Coding"
+
+# Task 2: Write a 2 line joke with a blank line in between
+
+######################################################################################
+# VARIABLES
+
+✅ first_name​
+
+✅ age​
+
+❌ FirstName​
+
+❌ First Name​
+
+❌ 1st_name
+
+first_name = "Alex"
+
+print(f"Hello {first_name} how are you?")
+
+# notice the f - f means format variables in the string.
+# notice the { } around the variable.
+
+
+# practice
+
+colour = "blue"
+number = 7
+
+print(f"My favourite colour is {colour}!")
+
+print(f"My lucky number is {number}.")
+
+
+##########################################################################
+# input
+
+name = input("What is your name? ")
+
+# input a number
+age = int(input("What is your age? "))
+# notice int
+# notice int(       ) around a normal input
+# notice the end ))
+
+# using numeric input
+print(f"Hi {name} I am older than you, i am {age + 1} years old.")
+
+
+# Task 3
+# ask the user what country they live in.
+# print out a response using their input
+# eg: user inputs 'Australia'
+# output: Wow, I live in Australia too!  (using the variable)
+
+# Task 4
+# ask the user for two numbers (use 2 input statements)
+# output the sum of the two numbers using the variables
+
+
+
